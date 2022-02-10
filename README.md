bc0761f4039de74b08a8be762234f25bfec8cb69
MarioCodi <mario.cortes@byjusfutureschool.com>
2022-02-09 23:48:14 -0600

    Traducción 1.0
    Modificación de comentarios y elementos en pantalla. Creación de README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# PROC105-V1-actividad-maestra3
+Python. OpenCV.  
+Código de referencia para la actividad de a maestra.  
+  
+Lesson plan.  
+  
+### Texto en inglés: PRO-105-Reference-Code-Teacher-Activity
--- Read_Video.py
+++ Read_Video.py
@@ -1,12 +1,12 @@
 import cv2
 
-#Use 0 for webcam
+# Usar "0" para la cámara web
 #vid = cv2.VideoCapture(0)
 
 vid = cv2.VideoCapture("AnthonyShkraba.mp4")
 
 if(vid.isOpened()==False):
-    print("Unable to read the feed")
+    print("No es posible leer la entrada.")
 
 
 height  = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -20,11 +20,11 @@ out = cv2.VideoWriter('Boxing.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 30, (width,h
 
 while(True):
       
-    # Capture the video frame
-    # by frame
+    # Captura el video
+    # cuadro a cuadro
     ret, frame = vid.read()
 
-    cv2.imshow("Web cam", frame)
+    cv2.imshow("Camara web", frame) #cv2 no acepta caracteres especiales como el acento en "Cámara"
     out.write(frame)
     if cv2.waitKey(25) == 32:
         break
